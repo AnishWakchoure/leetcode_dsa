92fd4cf8839289677c84dcfba2a1b51d3db48ae5
Anish Wakchoure <149792581+AnishWakchoure@users.noreply.github.com>
2026-08-22 18:50:03 +0530

    Solved this Q after it was asked in mock interview. I 1st solved it using a new string, then optimized it.

---
+++ Arrays/344_Reverse_String.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size()-1;
+
+        while(left<right){
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+
+
+/*
+string s = "anish";
+string result = "";
+
+for (int i=0; i<s.length(); i++){
+    result = s[i] + result;
+}
+cout<<result<<endl;
+*/
