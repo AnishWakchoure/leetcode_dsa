9131107b4607814d740c0b83aadb89cf0ec795c6
Anish Wakchoure <149792581+AnishWakchoure@users.noreply.github.com>
2026-08-24 16:19:04 +0530

    Implemented Boyer–Moore Majority Vote Algorithm

---
+++ Algorithms/Boyer_Moore_Majority_Vote.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int vote = 0;
+        int candidate = 0;
+        
+        //Find candidate
+        for (int i : nums){
+            if (vote == 0){
+                candidate = i;
+                vote = 1;
+            }
+            else if (i == candidate){
+                vote++;
+            }
+            else{
+                vote--;
+            }
+        }
+        
+        //Verify candidate - Majority exists
+        int count = 0;
+        for(int i : nums){
+            if (i == candidate){
+                count++;
+            }
+        }
+        //A majority element means it appears more than half of the array
+        if (count > nums.size()/2){
+            return candidate;
+        }
+        return -404;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Example input
+    vector<int> nums = {1, 2, 1};
+
+    int result = sol.majorityElement(nums);
+
+    if (result != -404)
+        cout << "Majority element is: " << result << endl;
+    else
+        cout << "Error 404. No majority element found." << endl;
+
+    return 0;
+}
