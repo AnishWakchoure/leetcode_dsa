7424647ad9f13ea1cbd14813d5182e3ad2f2c647
Anish Wakchoure <149792581+AnishWakchoure@users.noreply.github.com>
2026-08-29 22:35:22 +0530

    Easisest Q on leecode :)

---
+++ Math/50_Pow(x, n).py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x, n)
+
+
+
+
+''' 
+
+C++ Soln
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x, n);
+    }
+};
+
+'''
