2ed3b3c3e83027aa81972aa51b85265d8ed66931
Anish Wakchoure <149792581+AnishWakchoure@users.noreply.github.com>
2026-09-04 21:11:42 +0530

    Solved leetcode 14 using another, easy to read and understand approach.

--- Strings/14_Longest_Common_Prefix.py
+++ Strings/14_Longest_Common_Prefix.py
@@ -7,3 +7,33 @@ class Solution:
                     return res
             res += strs[0][i]
         return res
+
+
+#idr above soln, but below is the latest one.
+
+
+'''
+Sort the strings:
+Sorting arranges all strings in lexicographical order. After sorting, the first string and the last string will be the most different.
+→ The longest common prefix of the entire list must also be a prefix of these two extremes.
+
+Compare first and last strings:
+Loop through characters of the first string and compare them with the corresponding characters of the last string.
+
+If they match → add to the prefix.
+
+If they differ → stop immediately, because no further common prefix exists.
+
+Return the prefix:
+The accumulated characters form the longest common prefix.
+'''
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = sorted(strs)
+        ans = ""
+        for i in range(len(strs[0])):
+            if (strs[0][i] != strs[len(strs)-1][i]):
+                return ans
+            ans += strs[0][i]
+        return ans
